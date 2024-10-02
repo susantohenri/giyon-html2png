a39e42624f76cd5f4ef593844703bdf86f7dcc15
Henri Susanto <henry.dinus@gmail.com>
2024-10-02 13:39:47 +0700

    init

---
+++ giyon-html2png.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Giyon HTML2PNG
+ * Plugin URI: https://github.com/susantohenri/giyon-html2png
+ * Description: Turn Woocommerce Order into Printable PNG.
+ * Version: 1.0.0
+ * Author: Henrisusanto
+ * Author URI: https://github.com/susantohenri/
+ * Text Domain: giyon-giyon-html2png
+ * Domain Path: /i18n/languages/
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ */
