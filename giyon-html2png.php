a73066883b23fe70955082ff3b3388e9882fe66e
Henri Susanto <henry.dinus@gmail.com>
2024-10-03 09:51:31 +0700

    ready to propose

--- giyon-html2png.php
+++ giyon-html2png.php
@@ -12,3 +12,19 @@
  * Requires at least: 6.5
  * Requires PHP: 7.4
  */
+
+add_filter('manage_woocommerce_page_wc-orders_columns', 'giyon_html2png_column_header');
+add_action('manage_woocommerce_page_wc-orders_custom_column', 'giyon_html2png_column_content', 10, 2);
+
+function giyon_html2png_column_header($columns)
+{
+    return array_merge($columns, ['giyon-html2png' => __('Cetak', 'textdomain')]);
+}
+
+function giyon_html2png_column_content($column_key, $order)
+{
+    if ($column_key == 'giyon-html2png') {
+        $href = plugin_dir_url(__FILE__) . 'invoice.php?order_id=' . $order->id;
+        echo "<a target='_blank' href='{$href}'>Cetak</a>";
+    }
+}
---
+++ invoice.php
@@ -0,0 +1,71 @@
+<?php
+
+include '../../../wp-load.php';
+$order_id = $_GET['order_id'];
+
+?>
+<html>
+
+<head>
+    <script type="text/javascript" src="<?= plugin_dir_url(__FILE__) . 'html2canvas.min.js' ?>"></script>
+    <style type="text/css">
+        tfoot {
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <table id="invoice" border="1">
+        <thead>
+            <tr>
+                <th>Produk</th>
+                <th>Jumlah</th>
+                <th>Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="2">Subtotal</th>
+                <th></th>
+            </tr>
+            <tr>
+                <th colspan="2">Pengiriman</th>
+                <th></th>
+            </tr>
+            <tr>
+                <th colspan="2">Metode Pembayaran</th>
+                <th></th>
+            </tr>
+            <tr>
+                <th colspan="2">Total</th>
+                <th></th>
+            </tr>
+        </tfoot>
+    </table>
+    <script type="text/javascript">
+        html2canvas(document.querySelector(`table`))
+            .then(function(canvas) {
+                document.body.appendChild(canvas)
+                const link = document.createElement('a')
+                link.download = 'invoice-<?= $order_id ?>.png'
+                link.href = document.querySelector(`canvas`).toDataURL()
+                link.click()
+                window.close()
+            })
+    </script>
+</body>
+
+</html>
